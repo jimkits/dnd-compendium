529d84d0e596a1dd9fe0572d0ea95996eb96c7fc
Dimitrios B <dimbit13@pm.me>
2026-05-27 17:53:21 +0100

    fix test errors

--- dnd-ui-tests/playwright.config.ts
+++ dnd-ui-tests/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        storageState: 'dnd-ui-tests/.auth/user.json',
+        storageState: authFile,
       },
       dependencies: ['setup'],
     },
